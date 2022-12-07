085fa5b7a30bb16c1b911561252990bf1928dbce
aman-ji-gupta <gamansantosh811@gmail.com>
2022-12-07 16:40:40 +0530

    done till send-reset-pass-email

---
+++ middleware/auth_middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const userSchema = require("../model/userSchema");
+const key = require("../controller/userController");
+
+const checkUserAuth = async (req , res , next) =>{
+    const {authorization} = req.headers;
+    let token;
+    if(authorization && authorization.startsWith("Bearer")){
+        try{
+            //get token from header
+            token = authorization.split(" ")[1];
+            //veryfy token
+            const {userID} = jwt.verify(token,key.secretKey);
+            //get user from token
+            req.userSchema = await userSchema.findById(userID).select('-password');
+            next();
+        }catch(err){
+            console.log(err);
+            res.status(401).json({
+                status : "Failed",
+                Message : "Unauthorized User..!"
+            })
+        }
+    }
+}
+
+module.exports={
+    checkUserAuth
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controller/userController");
 const validation = require("../validation/user/userValidation");
+const authorization = require("../middleware/auth_middleware");
 const {upload} = require("../middleware/imageStorage")
 
 router.post('/register',upload.single("profilepic"),validation.registerUserValidation ,user.userSignup);
 router.post('/login',validation.loginUserValidation ,user.userLogin);
+router.post('/send-reset-password-email',authorization.checkUserAuth,user.sendUserResetPasswordEmail);
 
 module.exports=router;
