c32401affa3f61760c58b15b6a75f1e889159e1f
aman-ji-gupta <gamansantosh811@gmail.com>
2022-12-07 15:10:34 +0530

    done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,4 @@ const user = require("../controller/userController");
 const validation = require("../validation/user/userValidation");
 const {upload} = require("../middleware/imageStorage")
 router.post('/register',upload.single("profilepic"),validation.registerUserValidation ,user.userSignup);
-
 module.exports=router;
